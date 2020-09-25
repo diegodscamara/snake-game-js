8fa8b5dc98ee343d6a2074b717e11764fb342041
Diego Câmara <diegodscamara@gmail.com>
2020-09-25 18:09:07 -0300

    set snake movements

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG() {
-    context.fillStyle = 'lightgren';
+    context.fillStyle = "lightgren";
     cotext.fillRect(0, 0, 16 * box, 16 * box);
 }
 
@@ -19,5 +20,25 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
